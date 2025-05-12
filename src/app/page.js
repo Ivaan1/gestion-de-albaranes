1782902f74246d36ccf1ea663d6c5d2197c64e45
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-12 20:39:57 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,10 @@
-import LoginForm from "../components/page/auth/LoginForm";
-import RegisterForm from "../components/page/auth/RegisterForm";
-import Link from "next/link";
-import RemoveAllCookies from "../components/RemoveAllCookies";
-import React from "react";
-
+import LoginForm from "@/components/page/auth/LoginForm";
 
 export default function Home() {
   
   return (
    <div>
   <LoginForm />
-  <RemoveAllCookies />
    </div>
   );
 }
