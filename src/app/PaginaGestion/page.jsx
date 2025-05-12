4bddf8de4e7ecc5ac7efe480af58b0adbd7f4f5e
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-12 21:27:00 +0200

    pagina de gestion principal done

--- src/app/PaginaGestion/page.jsx
+++ src/app/PaginaGestion/page.jsx
@@ -8,52 +8,67 @@ import axios from 'axios';
 import Link from 'next/link';
 import Cookies from 'js-cookie';
 import { useRouter, useSearchParams } from 'next/navigation';
-import withAuth from "../Utils/withAuth";
+import { getClients } from "@/app/utils/api";
 
 
 const PaginaGestion = () => {
-  const searchParams = useSearchParams();
-  const userId = searchParams.get('userId');
-  const [clients, setClients] = useState([]); // Estado para los clientes
-  const [mensaje, setMessage ] = useState('');
-  const token = Cookies.get(`user_${userId}`); // Asegúrate de que el token esté almacenado con el userId
-  
- 
+
+  const [clientes, setClientes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-  
-    const fetchUserClients = async () => {
-      try {
-        const token = Cookies.get(`user_${userId}`); // Asegúrate de que el token esté almacenado con el userId
-  
-        const response = await axios.get(`https://bildy-rpmaya.koyeb.app/api/client`,{
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        });
-      
-        const fetchedClients = response.data;
-        if (fetchedClients.length === 0) {
-          setMessage("No tienes clientes aún, empieza por crear uno.");
-         
-        }
-  
-      } catch (error) {
+    const fetchClientes = async () => {
+      const token = Cookies.get("jwt") || localStorage.getItem("token");
+      if (!token) {
+        console.error("Token no encontrado");
+        return;
       }
+      //obtenemos los clientes 
+      const data = await getClients(token);
+
+      if (data) {
+        setClientes(data);
+      }
+      setLoading(false);
     };
-    fetchUserClients();
-  }, [userId]); // Ejecutar el efecto cuando `userId` cambie
-  
+
+    fetchClientes();
+  }, []);
+
   
   return (
-    <div className="text-center">   
+    <>
+    <main>
+        <h2 className=' mb-9 text-center text-2xl/9 font-bold tracking-tight text-gray-900'>Bienvenido</h2>
       
-      <div className="mt-5">
-      <h2 className=' mb-9 text-center text-2xl/9 font-bold tracking-tight text-gray-900'>Bienvenido</h2>
-        {mensaje && <p className="text-xl font-bold">{mensaje}</p>}
+        {clientes.length === 0 ? (
+        <div style={{ textAlign: "center", marginTop: "2rem" }}>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/4076/4076549.png"
+            alt="Sin clientes"
+            style={{ width: "150px", marginBottom: "1rem" }}
+          />
+          <p>Aún no tienes ningún cliente.</p>
+          <Link href="/PaginaGestion/CrearCliente">Crear un cliente ahora</Link>
+        </div>
+      ) : (
+        <div>
+        <button>
+          <Link href="/PaginaGestion/CrearCliente">Agregar cliente</Link>
+        </button>
+        <ul>
+          {clientes.map((cliente) => (
+            <li key={cliente.id}>
+              <p>{cliente.nombre}</p>
+                <Link href="/PaginaGestion/Clientes">Ver Detalles</Link>
+                <Link href="/PaginaGestion/Clientes">Editar</Link>
+            </li>
+          ))}
+        </ul>
       </div>
-      <Link href= {`/PaginaGestion/CrearCliente?userId=${userId}`} className="font-semibold text-indigo-600 hover:text-indigo-500">Crea un cliente ahora 
-      </Link>
-   </div>
+    )}
+    </main>
+   </>
   );
 }
-export default withAuth(PaginaGestion);
+export default PaginaGestion;
